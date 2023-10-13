e4aca40bbae19232adf91d16065933b3614c1264
jabezarandilla <144765600+jabezarandilla@users.noreply.github.com>
2023-10-13 17:23:45 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginModule } from './login/login.module';
+import { LayoutModule } from './layout/layout.module';
+import { RegistrationModule } from './registration/registration.module';
 
 
 
@@ -20,6 +24,10 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    LoginModule,
+    LayoutModule,
+    RegistrationModule,
+    NgbModule,
     
   ],
   providers: [],
